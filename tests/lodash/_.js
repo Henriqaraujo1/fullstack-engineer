0d04bb3afed3f232fec659339668ef9a6929be54
Henrique Silva <39538139+Henriqaraujo1@users.noreply.github.com>
2021-06-09 23:18:58 -0300

    refresh test codecademy

--- tests/lodash/_.js
+++ tests/lodash/_.js
@@ -4,12 +4,52 @@ const _ = {
         let clampedValue = Math.min(lowerClampedValue, upper)
 
         return clampedValue
-    }
-
+    },
+    inRange: function(number,start, end) {
+        if(end === undefined) {
+           end = start;
+           start = 0
+        } 
+          
+        if (start > end) {
+            const temp = end;
+            end = start;
+            start = temp
+        }
+        const isInRange =  start <= number && number < end 
+        return isInRange;
     
+    },
+    words: function(string) {
+        let words = string.split(' ')
+        return words
+    },
+    pad: function(string, length) {
+        if(length <= string.length) {
+            return string
+        }
+            const startPaddingLength = Math.floor((length - string.length)/2)
+            const endPaddingLength = length - string.length - startPaddingLength
+            const paddedString = ' '.repeat(startPaddingLength) + string + ' '.repeat(endPaddingLength)
+            return paddedString
+    },
+    has(object, key) { 
+        const hasValue = object[key]
+        if(hasValue != undefined){
+            return true
+        } else {
+            return false
+        }
+    },
+    invert(object) {
+        let invertedObject = {}
+        for (var key in object){
+            let originalValue = object[key]
+            invertedObject = {originalValue:key}
+        }
+        return invertedObject
+    },
 }
-
-
 
 
 // Do not write or modify code below this line.
