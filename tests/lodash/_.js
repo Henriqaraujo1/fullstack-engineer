44562aa8a2309b5fe3a5a755e2a1ad11a0d9848b
Henrique Silva <39538139+Henriqaraujo1@users.noreply.github.com>
2021-06-09 21:38:47 -0300

    test codecadame

---
+++ tests/lodash/_.js
@@ -0,0 +1,16 @@
+const _ = {
+    clamp: function(number, lower, upper) {
+        let lowerClampedValue = Math.max(number,lower);
+        let clampedValue = Math.min(lowerClampedValue, upper)
+
+        return clampedValue
+    }
+
+    
+}
+
+
+
+
+// Do not write or modify code below this line.
+module.exports = _;
