5a5d40ba5af43371ca16d3affcc6a6ad1427a001
Henrique <39538139+Henriqaraujo1@users.noreply.github.com>
2021-10-06 14:44:36 -0300

    teste1

--- projects/portfolio/js/smtp.js
+++ projects/portfolio/js/smtp.js
@@ -6,44 +6,55 @@ const sName = document.getElementById("sname");
 const email = document.getElementById("email");
 const phone = document.getElementById("phone");
 const message = document.getElementById("message");
+const buttonSubmit = document.getElementById("button");
 
-
-Email.send({
+const sendEmail =() =>{
+  Email.send({
     SecureToken : "b474e9ed-e0ea-4cc6-82e4-cdd12dce35a9b474e9ed-e0ea-4cc6-82e4-cdd12dce35a9",
     To : "henriqaraujo1@gmail.com",
     From : 'sendemail@henrique-silva.tech',
     Subject : "Contact for a Job",
     Body: "This is the contact from a Employer",
         Attachments : [
-
+            Name = fNome.value,
+            sName = sName.value,
+            email = email.value,
+            phone = phone.value,
+            message = message.value
         ]
-})
+});
+}
 
 
-const token = "e2b8d5d2-cd2c-4b19-b292-db72965ff884";
-const $formContact = document.querySelector("#contact");
+buttonSubmit.addEventListener('click', sendEmail);
 
-const sendForm = event => {
-  event.preventDefault();
-  const message = {
-    name: document.querySelector("#form-name").value,
-    subject: document.querySelector("#form-subject").value,
-    text: document.querySelector("#form-message").value
-  };
-  smtpJS(message);
-};
-const smtpJS = message => {
-  try {
-    Email.send(
-      "raulfdm@hotmail.com",
-      "devtesteapps@gmail.com",
-      `${message.name} - ${message.subject}`,
-      message.text,
-      { token }
-    );
-  } catch (e) {
-    alert("Erro");
-  }
-};
 
-$formContact.addEventListener("submit", sendForm);
+
+
+// const token = "e2b8d5d2-cd2c-4b19-b292-db72965ff884";
+// const $formContact = document.querySelector("#contact");
+
+// const sendForm = event => {
+//   event.preventDefault();
+//   const message = {
+//     name: document.querySelector("#form-name").value,
+//     subject: document.querySelector("#form-subject").value,
+//     text: document.querySelector("#form-message").value
+//   };
+//   smtpJS(message);
+// };
+// const smtpJS = message => {
+//   try {
+//     Email.send(
+//       "raulfdm@hotmail.com",
+//       "devtesteapps@gmail.com",
+//       `${message.name} - ${message.subject}`,
+//       message.text,
+//       { token }
+//     );
+//   } catch (e) {
+//     alert("Erro");
+//   }
+// };
+
+// $formContact.addEventListener("submit", sendForm);
