365575826ac2b6499ee4cb994b074886919834f2
Henrique Silva <henriqaraujo1@gmail.com>
2021-10-05 23:57:42 -0300

    fineshed the css and html
    Tomorrow Create the function to send the contact form

--- projects/portfolio/js/script.js
+++ projects/portfolio/js/script.js
@@ -14,3 +14,24 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+//Form
+
+const fNome = document.getElementById("fname");
+const sName = document.querySelector(".sname");
+const email = document.querySelector(".email");
+const phone = document.querySelector(".phone");
+const message = document.querySelector(".message");
+const buttonSend = document.querySelector(".button");
+
+const sendEmail = () => {
+    if(fNome.value != "") {
+        alert(`Thanks sr(a) ${fNome.value} ${sName.value} your e-mail has been forwarded sucessfully`)
+    }
+}
+// const sendEmail= () =>{
+//     alert("Your e-mail was sent thanks, I will contact you soon.");
+// }
+
+buttonSend.addEventListener("click", sendEmail);
+
