90a73265574cec9634814ae25ab10df70eefed8e
Henrique Silva <henriqaraujo1@gmail.com>
2022-01-25 00:32:52 -0300

    55/99 tasks done from codecademy project jammming

--- projects/jammming/src/Components/App/App.js
+++ projects/jammming/src/Components/App/App.js
@@ -1,21 +1,59 @@
+import React from 'react';
 import './App.css';
 import SearchBar from '../SearchBar/SearchBar'
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
-function App() {
-  return (
-    <div>
-      <h1>Ja<span class="highlight">mmm</span>ing</h1>
-      <div class="App">
-        <SearchBar />
-        <div class="App-playlist">
-          <SearchResults />
-          <Playlist />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchResults: [{ name: 'lucas', artist: 'lucas2', album: 'lucas3', id: 1 },
+      { name: 'pedro', artist: 'pedro2', album: 'pedro3', id: 2 },
+      { name: 'marcos', artist: 'marcos2', album: 'marcos3', id: 3 }],
+      playlistName: 'New Playlist',
+      playlistTracks: [{ name: 'lucas5', artist: 'lucas2', album: 'lucas3', id: 1 },
+      { name: 'pedro5', artist: 'pedro2', album: 'pedro3', id: 2 },
+      { name: 'marcos5', artist: 'marcos2', album: 'marcos3', id: 3 }]
+    };
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+  }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if(tracks.find(savedTrack => savedTrack.id === track.id)){
+      return;
+    }
+
+    tracks.push(track);
+    this.setState({playlistTracks : tracks})
+  }
+
+  removeTrack(track){
+    let tracksOut = this.state.playlistTracks;
+    tracksOut = tracksOut.filter(removedTrack => removedTrack.id !== track.id)
+
+    this.setState({playlistTracks : tracksOut})
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar />
+          <div className="App-playlist">
+            <SearchResults searchResult={this.state.searchResults} 
+                          onAdd={this.addTrack}/>
+            <Playlist playlistTracks={this.state.playlistTracks}
+                      onRemove={this.removeTrack}/>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- projects/jammming/src/Components/TrackList/TrackList.js
+++ projects/jammming/src/Components/TrackList/TrackList.js
@@ -1,13 +1,25 @@
-import { Component } from "react";
-import './TrackList.css'
-import Track from "../Track/Track";
+import React from 'react';
+import './TrackList.css';
+import Track from '../Track/Track';
 
-export default class TrackList extends Component {
+class TrackList extends React.Component {
     render() {
         return (
             <div className="TrackList">
-              <Track />  
+                {
+                    this.props.tracks.map(track => {
+                        return (
+                        <Track track={track} 
+                               key={track.id}
+                               onAdd={this.props.onAdd}
+                               onRemove={this.props.onRemove}
+                               isRemoval={this.props.isRemoval}/>)
+
+                    })
+                }
             </div>
         )
     }
 }
+
+export default TrackList;
