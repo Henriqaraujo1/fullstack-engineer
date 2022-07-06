157abc00dce3ff31614e54cfe82567dbfea7d5ba
Henrique <henriqaraujo1@gmail.com>
2022-07-05 23:33:23 -0300

    projeto finalizado

--- projects/back-end/quote-api-starting/server.js
+++ projects/back-end/quote-api-starting/server.js
@@ -1,4 +1,4 @@
-const { application } = require("express");
+const { application, query } = require("express");
 const express = require("express");
 const app = express();
 
@@ -13,6 +13,21 @@ app.listen(PORT, () => {
   console.log(`The server is Running in ${PORT}`);
 });
 
+app.get("/api/quotes", (req, res, next) => {
+  let findAuthor = req.query.person;
+  if (findAuthor === undefined) {
+    res.send({
+      quotes: quotes,
+    });
+  } else if(findAuthor) {
+    const quotesFind = quotes.filter(author => author.person === findAuthor)
+    res.send({quotes: quotesFind})
+  } else {
+    res.status(404).send('Sorry not found');
+  }
+});
+
+
 app.get("/api/quotes/random", (req, res, next) => {
   const quotesRandom = getRandomElement(quotes);
   if (quotesRandom) {
@@ -25,27 +40,15 @@ app.get("/api/quotes/random", (req, res, next) => {
   }
 });
 
-app.get("/api/quotes/", (req, res, next) => {
-  const personFind = req.query.person;
-  const qoutesFind = [];
-  const id = 0;
-  if (personFind) {
-    quotes.forEach((quote, id) => {
-      if ((quote.person = personFind)) {
-        qoutesFind.push({
-          id: id,
-          quote: quote.quote,
-          person: personFind
-        });
-      }
-    });
-  } else {
-    quotes.forEach((quote) => {
-      qoutesFind.push(quote.quote);
-    });
-  }
-  const returnArray = {
-    quotes: qoutesFind,
-  };
-  res.send(returnArray);
+app.post("/api/quotes", (req, res, next) => {
+  const newText = req.query.quote;
+  const newPerson = req.query.person;
+  if (newText.length >= 0  && newPerson.length >= 0) {
+    const newQoutes = {
+      quote: newText,
+      person: newPerson,
+    };
+    quotes.push(newQoutes)
+    res.send({quote: newQoutes})
+  } 
 });
